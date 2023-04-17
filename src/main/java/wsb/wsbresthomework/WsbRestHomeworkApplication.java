3357879220a3688e51aa4d5398b87a6914b685e3
Adrian <pogorzelski.adrian@gmail.com>
2023-04-17 21:37:21 +0200

    Initial commit

---
+++ src/main/java/wsb/wsbresthomework/WsbRestHomeworkApplication.java
@@ -0,0 +1,13 @@
+package wsb.wsbresthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbRestHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsbRestHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsb/wsbresthomework/WsbRestHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.wsbresthomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbRestHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
